90729edefc3ac36739193e7ad0fdbfcecf072202
Danilo Bezerra <86009742+danilo-bezerra@users.noreply.github.com>
2022-07-28 01:46:18 -0300

    versão quase pronta

--- src/services/api.js
+++ src/services/api.js
@@ -7,5 +7,5 @@ export const publicKey = "099fd44fbbbed9613716df1c5b100d96";
 export const md5Hash = "64a32d78ec843395a39f859d21fc044c";
 
 export const api = axios.create({
-  baseURL: "http://gateway.marvel.com/v1/public/",
+  baseURL: "https://gateway.marvel.com/v1/public/",
 });
